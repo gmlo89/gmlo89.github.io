cea9d01c1861e004643d544c9f575b3a200ded50
Giovanni Lopez <gmlo89@hotmail.com>
2022-04-29 14:13:52 -0600

    wip

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://gmlo89.github.io/',
   plugins: [vue()],
   resolve: {
     alias: {
